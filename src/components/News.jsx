ae787a13e2c2f2fdc70da3e5a2dab8df6c31895a
Prakhar <katariyaprakhar1@gmail.com>
2022-05-02 00:26:31 +0530

    news changes

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,47 +1,28 @@
-import React, { useState } from 'react';
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
-import moment from 'moment';
-
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import React from 'react';
+import { Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment'; 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
 
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
-const { Text, Title } = Typography;
-const { Option } = Select;
+const { Text, Title } = Typography; 
 
-const News = ({ simplified }) => {
-  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  const { data } = useGetCryptosQuery(100);
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+const News = () => { 
+const newsCategory = 'Cryptocurrency'
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory });
 
   if (!cryptoNews?.value) return <Loader />;
 
   return (
-    <Row gutter={[24, 24]}>
-      {!simplified && (
-        <Col span={24}>
-          <Select
-            showSearch
-            className="select-news"
-            placeholder="Select a Crypto"
-            optionFilterProp="children"
-            onChange={(value) => setNewsCategory(value)}
-            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-          >
-            <Option value="Cryptocurency">Cryptocurrency</Option>
-            {data?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
-          </Select>
-        </Col>
-      )}
+    <Row gutter={[24, 24]}> 
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
                 <Title className="news-title" level={4}>{news.name}</Title>
-                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} style={{maxWidth: '200px', maxHeight:'100px'}} alt="" />
               </div>
               <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
               <div className="provider-container">
